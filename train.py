b42c1ae1801e2b53942d38b0e7517c236e66032f
wangning1996chris <chris1996wangning@gmail.com>
2024-04-18 21:59:10 +0800

    change ppo

--- train.py
+++ train.py
@@ -1,35 +1,31 @@
 # Env humanoid
 from elegantrl.train.run import train_agent, train_agent_multiprocessing
 from elegantrl.train.config import Config, build_env, get_gym_env_args
-from elegantrl.agents import AgentDQN
-
-agent_class = AgentDQN
-env_name = "CartPole-v0"
-
+from elegantrl.agents import AgentPPO
+from elegantrl.envs.CustomGymEnv import PendulumEnv
 import gym
-gym.logger.set_level(40)  # Block warning
-env = gym.make(env_name)
-env_func = gym.make
-env_args = get_gym_env_args(env, if_print=True)
 
+gym.logger.set_level(40)  # Block warning
+env = PendulumEnv()
+env_func = PendulumEnv
+agent_class = AgentPPO
+env_args = get_gym_env_args(env, if_print=True)
 args = Config(agent_class, env_func, env_args)
 
-'''network train'''
-args.max_step = 1000
-
-'''reward shaping'''
-args.reward_scale = 2 ** 0  # an approximate target reward usually be closed to 256
-args.gamma = 0.97  # discount factor of future rewards
+args.reward_scale = 2 ** -1  # RewardRange: -1800 < -200 < -50 < 0
+args.gamma = 0.97
 
 '''network update'''
-args.batch_size = 2 ** 7  # num of transitions sampled from replay buffer.
-args.repeat_times = 2 ** 0  # repeatedly update network using ReplayBuffer to keep critic's loss small
+args.target_step = args.max_step * 8
+args.net_dim = 2 ** 7
+args.num_layer = 2
+args.batch_size = 2 ** 8
+args.repeat_times = 2 ** 5
 
 '''evaluate'''
-args.eval_times = 2 ** 3  # number of times that get episode return
-args.break_step = int(8e4)  # break training if 'total_step > break_step'
+args.eval_gap = 2 ** 6
+args.eval_times = 2 ** 3
+args.break_step = int(8e5)
 
-'''gpu cpu'''
 args.learner_gpus = -1
-
 train_agent(args)
